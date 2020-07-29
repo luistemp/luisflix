5b2ad3614ce672f577f314310beba93aaa09f264
unknown <nestlaper@yahoo.com.br>
2020-07-29 18:02:36 -0300

    Setup inicial

---
+++ src/components/Menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import Button from '../Button'
+//import ButtonLink from './components/ButtonLink';
+import './Menu.css';
+
+function Menu() {
+    return <nav className="Menu">
+        <a href="/">
+            <img className="Logo" src={Logo} alt="Luisflix logo"/>
+        </a>
+        <Button as="a" className="ButtonLink" href="/">
+            Novo vídeo
+        </Button>
+    </nav>
+}
+
+export default Menu;
